16972918ca761b4d985ff766b69dac34d9a391b3
Boris Guryev <boris.gurev@x5.ru>
2021-10-14 20:04:43 +0300

    lift speed up and tests

--- src/controller.py
+++ src/controller.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+from impl.utils.lqr import lqr
+
+
 class Controller:
     def __init__(self):
         pass
@@ -40,3 +45,35 @@ class SquareWaveController(Controller):
 
     def control(self, state, t):
         return self.A * (-1) ** round(t / self.per)
+
+
+class LQR(Controller):
+    def __init__(self, x0, A, B, u0, Q=None, R=None, Umax=None, lift=lambda x: x, discr=False):
+        super().__init__()
+        self.lift = lift
+        self.Umax = Umax
+        self.x0 = np.array(x0)
+        self.x0_lifted = np.array(lift(x0))
+        self.A = A
+        self.B_inv = np.linalg.pinv(B)
+        self.u0 = u0
+        self.discr = discr
+
+        if Q is None:
+            Q = np.eye(A.shape[1])
+        if R is None:
+            R = np.eye(B.shape[1])
+        _, K, _ = lqr(A, B, Q, R, discr=discr)
+        self.K = K
+
+    def control(self, state, t):
+        """
+        Срань захардкоженная. если будет баговать - переписать
+        """
+        state = np.array(state[:])
+        state = self.lift(state.reshape(self.x0.shape))
+        u = self.u0 + self.K.dot(state - self.x0_lifted).flatten()
+        if self.Umax:
+            return np.sign(u) * min(abs(u), self.Umax)
+        else:
+            return u
